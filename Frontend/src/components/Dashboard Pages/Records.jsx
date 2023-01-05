0bc4e3f3e73f0ce38cfa1fc430bc20530deea345
HLrahul <rahuls10org@gmail.com>
2023-01-05 15:44:40 +0530

    edit and delete records

--- Frontend/src/components/Dashboard Pages/Records.jsx
+++ Frontend/src/components/Dashboard Pages/Records.jsx
@@ -1,8 +1,9 @@
 // Imports
-import { format } from "date-fns";
 import { useState, useEffect, useRef } from "react";
-import useAxiosPrivate from "../../hooks/usePrivateAxios";
 import styled from "styled-components";
+import useAxiosPrivate from "../../hooks/usePrivateAxios";
+import ReadOnlyRow from "./ReadOnlyRow";
+import EditableRow from "./EditableRow";
 
 // Records api URL
 const RECORDS_URL = "";
@@ -12,12 +13,9 @@ function Records() {
   // Constants for the states and miscallaneous
   const [records, setRecords] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [editId, setEditId] = useState(null);
   const dataFetchedRef = useRef(false);
   const axiosPrivate = useAxiosPrivate();
-
-  // Options for Time and Date formatting
-  const timeOptions = { hour: "numeric", minute: "numeric", hour12: true };
-  const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
 
   // Async Await axios request to the API for getting the records
   const getRecords = async () => {
@@ -39,49 +37,6 @@ function Records() {
     getRecords();
   }, []);
 
-  // Function to render the Table records in the table
-  function renderTableRecord() {
-    return records.map((record, index) => {
-      const {
-        courier_number,
-        courier_type,
-        courier_company,
-        from_company,
-        to_company,
-        to_destination,
-        courier_weight,
-        courier_rate,
-        phone_no,
-        booked_date,
-        booked_time,
-      } = record;
-
-      // Formatting Date
-      const formattedDate = format(new Date(booked_date), "dd-MM-yy");
-      // Formatting Time
-      const [hours, minutes, seconds] = booked_time.split(":");
-      const time = new Date(0, 0, 0, hours, minutes, seconds);
-      const timeFormatter = new Intl.DateTimeFormat("en-US", timeOptions);
-      const formattedTime = timeFormatter.format(time);
-
-      return (
-        <Tr key={index}>
-          <Td data-label="C.Number">{courier_number}</Td>
-          <Td data-label="C.Type">{courier_type}</Td>
-          <Td data-label="C.Company">{courier_company}</Td>
-          <Td data-label="From">{from_company}</Td>
-          <Td data-label="To">{to_company}</Td>
-          <Td data-label="Destination">{to_destination}</Td>
-          <Td data-label="Weight">{courier_weight}</Td>
-          <Td data-label="Amount">{courier_rate}</Td>
-          <Td data-label="Phone">{phone_no}</Td>
-          <Td data-label="Date">{formattedDate}</Td>
-          <Td data-label="Time">{formattedTime}</Td>
-        </Tr>
-      );
-    });
-  }
-
   // Actual records JSX element.
   return (
     <RecordsWrapper>
@@ -102,8 +57,26 @@ function Records() {
               <Th>Phone</Th>
               <Th>Date</Th>
               <Th>Time</Th>
+              <Th>Edit</Th>
+              <Th>Delete</Th>
             </Tr>
-            {renderTableRecord()}
+            {records.map((record, index) => {
+              return editId === record.courier_number ? (
+                <EditableRow
+                  record={record}
+                  index={index}
+                  setEditId={setEditId}
+                  getRecords={getRecords}
+                />
+              ) : (
+                <ReadOnlyRow
+                  record={record}
+                  index={index}
+                  setEditId={setEditId}
+                  getRecords={getRecords}
+                />
+              );
+            })}
           </Tbody>
         </RecordsTable>
       )}
@@ -113,6 +86,18 @@ function Records() {
 export default Records;
 
 // Stylings
+const RecordsWrapper = styled.div`
+  padding-top: 5vh;
+  min-height: 100vh;
+  width: 100%;
+  background: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  transition: 0.3s ease;
+`;
+
 const P = styled.div`
   height: 100vh;
   width: 100%;
@@ -124,6 +109,7 @@ const P = styled.div`
 `;
 
 const RecordsTable = styled.table`
+  font-size: 0.85rem;
   width: 90%;
   border-collapse: collapse;
 
@@ -164,39 +150,4 @@ const Th = styled.th`
   @media (max-width: 425px) {
     display: none;
   }
-`;
-
-const Td = styled.td`
-  padding: 10px;
-  text-align: center;
-
-  @media (max-width: 425px) {
-    color: white;
-    display: block;
-    text-align: right;
-    position: relative;
-    padding-left: 50%;
-
-    &::before {
-      content: attr(data-label);
-      color: grey;
-      position: absolute;
-      left: 0;
-      width: 50%;
-      font-weight: 900;
-      text-align: left;
-      padding-left: 10px;
-    }
-  }
-`;
-
-const RecordsWrapper = styled.div`
-  padding-top: 5vh;
-  min-height: 100vh;
-  width: 100%;
-  background: black;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: white;
 `;
