f3284918e13c47176a6698b238a8742cbe169864
HLrahul <rahuls10org@gmail.com>
2023-01-05 15:44:40 +0530

    edit and delete records

--- Frontend/src/hooks/usePrivateAxios.jsx
+++ Frontend/src/hooks/usePrivateAxios.jsx
@@ -1,22 +1,17 @@
 // Imports
 import { axiosPrivate } from "../api/axios";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import RefreshToken from "./RefreshToken";
 
 // Functional component creating custom hook useAxiosPrivate
 const useAxiosPrivate = () => {
-  const refreshRef = useRef(false);
-
   // accessToken from store and refresh instance from refreshToken component
   const accessToken = useSelector((state) => state.user.userAccessToken);
   const refresh = RefreshToken();
 
   // Actual logic to integrate the bearer token in the header for authorization
   useEffect(() => {
-    if (refreshRef.current) return;
-    refreshRef.current = true;
-
     const requestIntercept = axiosPrivate.interceptors.request.use(
       (config) => {
         if (!config.headers["Authorization"]) {
@@ -41,8 +36,6 @@ const useAxiosPrivate = () => {
       }
     );
 
-    console.log("i m running!");
-    // cleanup function to prevent piling of the above functions
     return () => {
       axiosPrivate.interceptors.request.eject(requestIntercept);
       axiosPrivate.interceptors.response.eject(responseIntercept);
