318dcd5c8f74026f96f1364bb5229030b07dd5c5
GoAmeer030 <59198817+GoAmeer030@users.noreply.github.com>
2023-01-10 02:57:00 +0000

    test

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 // const BASE_URL = `http://${import.meta.env.VITE_BACKEND_IP_ADDRESS}/api/`;
 // const BASE_URL = "http://127.0.0.1:8000/api/";
 // const BASE_URL = "https://goameer030-animated-garbanzo-pw66w6q777wfr56v-8000.preview.app.github.dev/api/";
-const BASE_URL = "http://backend:8000/api/"
+// const BASE_URL = "http://backend:8000/api/"
+const BASE_URL = "https://backend-goameer030.cloud.okteto.net/api/"
 
 export default axios.create({
   baseURL: BASE_URL,
