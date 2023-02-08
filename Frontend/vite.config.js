e6bc87d21bc9ae9184bc6186e8f7991a158f6d7c
HLrahul <rahuls10org@gmail.com>
2023-02-08 16:48:54 +0530

    Resolved vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +11,6 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173,
-}})
+    disableDisconnectRefresh: true,
+  },
+});
